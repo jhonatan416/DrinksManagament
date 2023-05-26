851d9948636c4b999decbcc1323baeaa641e6d0b
Jhonatan Olmos <jhonatan.olmos@softtek.com>
2023-05-26 09:15:39 -0500

    Agregar archivos de proyecto.

---
+++ DrinksManagament/Data/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DrinksManagament.Data
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal PriceForOne { get; set; }
+        public decimal PriceForStock { get; set; }
+
+        [ForeignKey("ProductTypeId")]
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+    }
+}
---
+++ DrinksManagament/Data/ProductType.cs
@@ -0,0 +1,7 @@
+﻿namespace DrinksManagament.Data
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
